add33a9cd33203ed9936d67444586a579b1c6703
aromanet <audrey.romanet42@gmail.com>
2016-07-01 16:21:44 +0200

    first commit

---
+++ src/main/java/com/example/ServiceController.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/service")
+public class ServiceController {
+
+    @RequestMapping(value = "", method = RequestMethod.GET,
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public Service getMyService() {
+        return new Service("DFKEFJ", "DSISDKF");
+    }
+
+}
